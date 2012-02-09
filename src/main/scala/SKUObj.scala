b470e3531e3cc4ca8a635d4cf544ca57a94e5d88
Moses <nnythm@gmail.com>
2012-02-09 12:05:52 -0500

    style changes

--- src/main/scala/SKUObj.scala
+++ src/main/scala/SKUObj.scala
@@ -1,2 +1,3 @@
-case class SKUObj(val id: Number, val inventory_status: String, val msrp_price: String, val sale_price: String, 
-									val shipping_surcharge: Option[String], val attributes: Map[String,String])
+case class SKUObj(val id: Number, val inventory_status: String, val msrp_price: String,
+                  val sale_price: String, val shipping_surcharge: Option[String],
+                  val attributes: Map[String, String])
