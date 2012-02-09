dafad1ba9d3af3c829c1e5d9c1f67a14a4999cb3
Moses <nnythm@gmail.com>
2012-02-09 11:51:34 -0500

    added tests, works like a boss

--- src/main/scala/GiltClient.scala
+++ src/main/scala/GiltClient.scala
@@ -17,9 +17,12 @@ class GiltClient(val apiKey: String) {
     })
   }
 
-/*  def active(store_key: String): List[SaleObject] = {
+  def active(store_key: String): List[SaleObject] = {
+    if (!validStore(store_key)) {
+      throw new IllegalArgumentException("Not a valid store key: " + store_key)
+    }
     val h = new Http
-    val req = url("https://api.gilt.com/v1/sales/" + store_key + "active.json")
+    val req = url("https://api.gilt.com/v1/sales/" + store_key + "/active.json")
     h(req <<? Map("apikey" -> apiKey) ># {json =>
       getSaleObject(json)
     })
@@ -27,15 +30,18 @@ class GiltClient(val apiKey: String) {
 
   def upcoming: List[SaleObject] = {
     val h = new Http
-    val req = :/("https://api.gilt.com/v1/sales/upcoming.json")
+    val req = url("https://api.gilt.com/v1/sales/upcoming.json")
     h((req <<? Map("apikey" -> apiKey)) ># {json =>
       getSaleObject(json)
     })
   }
 
   def upcoming(store_key: String): List[SaleObject] = {
+    if (!validStore(store_key)) {
+      throw new IllegalArgumentException("Not a valid store key: " + store_key)
+    }
     val h = new Http
-    val req = :/("https://api.gilt.com/v1/sales/") / store_key / "upcoming.json"
+    val req = url("https://api.gilt.com/v1/sales/" + store_key + "/upcoming.json")
     h(req <<? Map("apikey" -> apiKey) ># {json =>
       getSaleObject(json)
     })
@@ -43,15 +49,17 @@ class GiltClient(val apiKey: String) {
 
   def detail(store_key: String, sale_key: String): SaleObject = {
     val h = new Http
-    val req = :/("https://api.gilt.com/v1/sales/") / store_key / sale_key / "detail.json"
+    val req = url("https://api.gilt.com/v1/sales/" + store_key + "/" + sale_key + "/detail.json")
     h(req <<? Map("apikey" -> apiKey) ># {json =>
       SaleObject(json.values.asInstanceOf[Map[String, Any]])
     })
-  }*/
+  }
 
-  def getSaleObject(j: JValue): List[SaleObject] = {
+  private[this] def getSaleObject(j: JValue): List[SaleObject] = {
     val innerLst = j.values.asInstanceOf[Map[String, List[Map[String, Any]]]]("sales")
     innerLst map (elt => SaleObject(elt))
   }
+
+  private[this] def validStore(store: String): Boolean = stores contains store
 }
 
--- src/test/scala/ClientTest.scala
+++ src/test/scala/ClientTest.scala
@@ -8,4 +8,35 @@ class ClientTest extends FunSuite {
     assert(!(tmp isEmpty))
   }
 
+  test("Active sales are received properly for all stores") {
+    val client = new GiltClient(Secret.password)
+    for (store <- client.stores) {
+      val tmp = client.active(store)
+      assert(!(tmp isEmpty))
+    }
+  }
+
+  test("Upcoming sales are received properly") {
+    val client = new GiltClient(Secret.password)
+    val tmp = client.upcoming
+    assert(!(tmp isEmpty))
+  }
+
+  test("Upcoming sales are received properly for all stores") {
+    val client = new GiltClient(Secret.password)
+    for (store <- client.stores) {
+      val tmp = client.upcoming(store)
+      assert(!(tmp isEmpty))
+    }
+  }
+
+  test("Details are working") {
+    val client = new GiltClient(Secret.password)
+    val sales = client.active
+    var lst: List[SaleObject] = Nil
+    for (sale <- sales) {
+      lst = client.detail(sale.store, sale.sale_key) :: lst
+    }
+    assert(!(lst isEmpty))
+  }
 }
