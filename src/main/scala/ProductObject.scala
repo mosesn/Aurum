bb7df29c23333e14f08f4d480eebdfe5f88de4e1
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-02-09 11:57:45 -0500

    Fixed ProductObject dumb errors

--- src/main/scala/ProductObject.scala
+++ src/main/scala/ProductObject.scala
@@ -8,16 +8,16 @@ object ProductObject {
 	def apply(map: Map[String, Any]): ProductObject ={
 		ProductObject((map("name")).toString,
 									(map("product")).toString,
-									(map("id")).asInstanceOf(Number),
+									(map("id")).asInstanceOf[Number],
 									(map("brand")).toString,
 									(map("url")).toString,
-									(map("image_urls")).asInstanceOf(List[ImageObj]),
-									(map("skus")).asInstanceOf(List[SKUObj]),
+									(map("image_urls")).asInstanceOf[List[ImageObj]],
+									(map("skus")).asInstanceOf[List[SKUObj]],
 									if (map.contains("description")) Some(map("description").toString) else None,
-									if (map.contains("fit_notes")) Some(map("fit_notes")).toString else None,
-									if (map.contains("material")) Some(map("material")).toString else None,
-									if (map.contains("care_instructions")) Some(map("care_instructions")).toString else None,
-									if (map.contains("origin")) Some(map("origin")).toString else None)
+									if (map.contains("fit_notes")) Some(map("fit_notes").toString) else None,
+									if (map.contains("material")) Some(map("material").toString) else None,
+									if (map.contains("care_instructions")) Some(map("care_instructions").toString) else None,
+									if (map.contains("origin")) Some(map("origin").toString) else None)
 	}
 	
 	
