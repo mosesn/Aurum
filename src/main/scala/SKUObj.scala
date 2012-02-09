80cc60a57cb48b99610161ea16f00051a3d0a2f3
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-02-09 11:56:32 -0500

    SKUObj.scala

--- src/main/scala/SKUObj.scala
+++ src/main/scala/SKUObj.scala
@@ -1,2 +1,2 @@
 case class SKUObj(val id: Number, val inventory_status: String, val msrp_price: String, val sale_price: String, 
-									val shipping_surcharge: Option[String], val attributes: List[AttributeObj])
+									val shipping_surcharge: Option[String], val attributes: Map[String,String])
