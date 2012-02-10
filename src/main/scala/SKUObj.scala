ed5f5c43a0aa0f6094733f6d2a6e1e3ac91221d2
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-02-09 23:51:20 -0500

    added apply stuff, but broken

--- src/main/scala/SKUObj.scala
+++ src/main/scala/SKUObj.scala
@@ -1,3 +1,14 @@
 case class SKUObj(val id: Number, val inventory_status: String, val msrp_price: String,
                   val sale_price: String, val shipping_surcharge: Option[String],
                   val attributes: Map[String, String])
+
+object SKUObj {
+	def apply(x: Map[String, Any]): SKUObj ={
+		SKUObj(x("id").asInstanceOf[Number], 
+		x("inventory_status").toString,
+		x("msrp_price").toString,
+		x("sale_price").toString,
+		if (x.contains("shipping_surcharge")) Some(x("shipping_surcharge").toString) else None,
+		if (x.contains("attributes")) Some(x("attributes").asInstanceOf[Map[String,String]]) else None)
+	}
+}
